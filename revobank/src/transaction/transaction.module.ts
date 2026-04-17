5d7c34fd822a652aeab793a93a6d20d49ff03478
stephenJoshua <149865909+stephen1234567890000000@users.noreply.github.com>
2026-04-17 17:50:18 +0700

    test

--- revobank/src/transaction/transaction.module.ts
+++ revobank/src/transaction/transaction.module.ts
@@ -4,6 +4,6 @@ import { TransactionController } from './transaction.controller';
 
 @Module({
   providers: [TransactionService],
-  controllers: [TransactionController]
+  controllers: [TransactionController],
 })
 export class TransactionModule {}
