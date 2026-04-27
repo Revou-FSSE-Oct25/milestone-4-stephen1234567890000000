77531e4e1d8c70b73de50e07aa9b307710e30a7e
stephenJoshua <149865909+stephen1234567890000000@users.noreply.github.com>
2026-04-27 13:36:03 +0700

    refactor: User prisma operation

--- revobank/src/user/user.controller.ts
+++ revobank/src/user/user.controller.ts
@@ -1,30 +1,23 @@
 import { Controller, Get, UseGuards, Req, Patch, Body } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt.authGuard';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from './user.service';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private userService: UserService) {}
 
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Req() req) {
-    return this.prisma.user.findUnique({
-      where: { id: req.user.id },
-    });
+    return this.userService.getProfile(req.user.id);
   }
 
   @UseGuards(JwtAuthGuard)
   @Patch('profile')
   updateProfile(@Req() req, @Body() body) {
-    return this.prisma.user.update({
-      where: { id: req.user.sub },
-      data: {
-        name: body,
-      },
-    });
+    return this.userService.updateProfile(req.user.sub, body);
   }
 }
--- revobank/src/user/user.service.ts
+++ revobank/src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getProfile(userId: string) {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+  }
+
+  async updateProfile(userId: string, data: any) {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data,
+    });
+  }
+}
